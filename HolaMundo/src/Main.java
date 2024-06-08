9d13fc53fe71f91b576beb8ceed9390577cc443d
Carlos Andres Ramirez <91298237+sacrabma92@users.noreply.github.com>
2024-06-08 11:56:03 -0500

    HolaMundo

---
+++ HolaMundo/src/Main.java
@@ -0,0 +1,10 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        String saludar = "Hola Mundo desde Java";
+        System.out.println("saludar.toUpperCase() = " + saludar.toUpperCase());
+
+        int numero = 10;
+    }
+}
