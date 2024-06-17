17e0dd6321075f895e92af6808cb82cef8a5c450
Carlos Andres Ramirez <91298237+sacrabma92@users.noreply.github.com>
2024-06-17 12:53:59 -0500

    FirsAPI

--- first-api-rest/src/main/java/com/example/first_api_rest/Controller/HelloController.java
+++ first-api-rest/src/main/java/com/example/first_api_rest/Controller/HelloController.java
@@ -1,4 +1,15 @@
 package com.example.first_api_rest.Controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
 public class HelloController {
+
+    @GetMapping("/hello")
+    public String saludar(){
+        return "Hola Mundo con REST en Spring Boot";
+    }
 }
