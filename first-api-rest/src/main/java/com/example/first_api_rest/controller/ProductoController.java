57c2d5513f6daef750c0f0be318e7cbbd2835f0f
Carlos Andres Ramirez <91298237+sacrabma92@users.noreply.github.com>
2024-06-17 22:01:36 -0500

    Creación del controller y probando todo es Postman

--- first-api-rest/src/main/java/com/example/first_api_rest/controller/ProductoController.java
+++ first-api-rest/src/main/java/com/example/first_api_rest/controller/ProductoController.java
@@ -1,7 +1,16 @@
 
 package com.example.first_api_rest.controller;
 
+import com.example.first_api_rest.model.Producto;
 import com.example.first_api_rest.service.ProductoService;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,6 +18,31 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/v1/productos")
 public class ProductoController {
     
+    @Autowired
     private ProductoService productoService;
     
+    @PostMapping
+    public Producto agregarProducto(@RequestBody Producto producto){
+        return productoService.saveProducto(producto);
+    }
+    
+    @GetMapping
+    public List<Producto> listarProductos(){
+        return productoService.getProductos();
+    }
+    
+    @GetMapping("/{id}")
+    public Producto buscarProducto(@PathVariable int id){
+        return productoService.getProductoById(id);
+    } 
+    
+    @PutMapping
+    public Producto actualizarProducto(@RequestBody Producto producto){
+        return productoService.updateProducto(producto);
+    }
+    
+    @DeleteMapping("/{id}")
+    public String eliminarProducto(@PathVariable int id){
+        return productoService.deleProducto(id);
+    }
 }
--- first-api-rest/src/main/java/com/example/first_api_rest/repository/ProductoRepository.java
+++ first-api-rest/src/main/java/com/example/first_api_rest/repository/ProductoRepository.java
@@ -1,6 +1,7 @@
 package com.example.first_api_rest.repository;
 
 import com.example.first_api_rest.model.Producto;
+import com.fasterxml.jackson.databind.util.ClassUtil;
 import org.springframework.stereotype.Repository;
 
 import java.util.ArrayList;
@@ -11,23 +12,6 @@ import java.util.stream.Collectors;
 public class ProductoRepository {
 
     private List<Producto> productos = new ArrayList<>();
-
-    // Creamos una lista de productos
-    public void createProducts(){
-        productos = List.of(
-                new Producto(1, "Producto 01", 2, 5),
-                new Producto(2, "Producto 02", 10, 900),
-                new Producto(3, "Producto 03", 10, 800),
-                new Producto(4, "Producto 04", 10, 600),
-                new Producto(5, "Producto 05", 10, 700),
-                new Producto(6, "Producto 06", 10, 400),
-                new Producto(7, "Producto 07", 10, 500),
-                new Producto(8, "Producto 08", 10, 300),
-                new Producto(9, "Producto 09", 10, 200),
-                new Producto(10, "Producto 10", 10, 100),
-                new Producto(11, "Producto 11", 10, 140)
-        );
-    }
 
     // Listar todos los productos
     public List<Producto> getAllProductos(){
