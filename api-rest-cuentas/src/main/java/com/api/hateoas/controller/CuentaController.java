94d874e0adbf8ac77aebf66a61cf654b167cf657
Carlos Andres Ramirez <91298237+sacrabma92@users.noreply.github.com>
2024-06-18 21:00:21 -0500

    Creación de del controller CuentaController - Parte 2

--- api-rest-cuentas/src/main/java/com/api/hateoas/controller/CuentaController.java
+++ api-rest-cuentas/src/main/java/com/api/hateoas/controller/CuentaController.java
@@ -7,7 +7,12 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -24,5 +29,37 @@ public class CuentaController {
             return ResponseEntity.noContent().build();
         }
         return new ResponseEntity<>(cuentas, HttpStatus.OK);
+    }
+    
+    @GetMapping("/{id}")
+    public ResponseEntity<Cuenta> listarCuenta(@PathVariable Integer id){
+        try {
+            Cuenta cuenta = cuentaService.get(id);
+            return new ResponseEntity<>(cuenta, HttpStatus.OK);
+        } catch (Exception e) {
+            return ResponseEntity.notFound().build();
+        }
+    }
+    
+    @PostMapping
+    public ResponseEntity<Cuenta> guardarCuenta(@RequestBody Cuenta cuenta){
+        Cuenta cuentaBBDD = cuentaService.save(cuenta);
+        return new ResponseEntity<>(cuenta, HttpStatus.CREATED);
+    }
+    
+    @PutMapping
+    public ResponseEntity<Cuenta> editarCuenta(@RequestBody Cuenta cuenta){
+        Cuenta cuentaBBDD = cuentaService.save(cuenta);
+        return new ResponseEntity<>(cuenta, HttpStatus.OK);
+    }
+    
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> eliminarCuenta(@PathVariable Integer id){
+        try {
+            cuentaService.delete(id);
+            return ResponseEntity.noContent().build();
+        } catch (Exception e) {
+            return ResponseEntity.notFound().build();
+        }
     }
 }
