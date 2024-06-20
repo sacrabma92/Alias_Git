8cb8223b4309f17f1e2b375cdf0f3814026ef35f
Carlos Andres Ramirez <91298237+sacrabma92@users.noreply.github.com>
2024-06-20 14:04:42 -0500

    Creación del controller EncuestaController

---
+++ api-rest-encuentas/src/main/java/com/api/rest/api_rest_encuentas/controller/EncuestaController.java
@@ -0,0 +1,62 @@
+package com.api.rest.api_rest_encuentas.controller;
+
+import com.api.rest.api_rest_encuentas.model.Encuesta;
+import com.api.rest.api_rest_encuentas.repository.EncuestaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+import java.util.Optional;
+
+@RestController
+public class EncuestaController {
+   @Autowired
+   private EncuestaRepository encuestaRepository;
+
+   @GetMapping("/encuestas")
+   public ResponseEntity<Iterable<Encuesta>> listarTodasLasEncuestas(){
+      return new ResponseEntity<>(encuestaRepository.findAll(), HttpStatus.OK);
+   }
+
+   @PostMapping("/encuestas")
+   public ResponseEntity<?> crearEncuetas(@RequestBody Encuesta encuesta){
+      encuesta = encuestaRepository.save(encuesta);
+
+      HttpHeaders httpHeaders = new HttpHeaders();
+      URI newEncuestaUri = ServletUriComponentsBuilder
+              .fromCurrentRequest()
+              .path("/{id}")
+              .buildAndExpand(encuesta.getId()).toUri();
+      httpHeaders.setLocation(newEncuestaUri);
+
+      return new ResponseEntity<>(null, HttpStatus.CREATED);
+   }
+
+   @GetMapping("/encuestas/{encuestaId}")
+   public ResponseEntity<?> obtenerEncuesta(@PathVariable Long encuestaId){
+      Optional<Encuesta> encuesta = encuestaRepository.findById(encuestaId);
+
+      if(encuesta.isPresent()){
+         return  new ResponseEntity<>(encuesta,HttpStatus.OK);
+      }else{
+         return new ResponseEntity<>(null,HttpStatus.NOT_FOUND);
+      }
+   }
+
+   @PutMapping("/encuestas/{encuetaId}")
+   public ResponseEntity<?> actualizarEncuesta(@RequestBody Encuesta encuesta, @PathVariable Long encuetaId){
+      encuesta.setId(encuetaId);
+      Encuesta e = encuestaRepository.save(encuesta);
+      return new ResponseEntity<>(HttpStatus.OK);
+   }
+
+   @DeleteMapping("/{encuestaId}")
+   public ResponseEntity<?> eliminarEncuesta(@PathVariable Long encuestaId){
+      encuestaRepository.deleteById(encuestaId);
+      return new ResponseEntity<>(HttpStatus.OK); 
+   }
+}
