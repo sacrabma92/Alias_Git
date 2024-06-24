2e5008196f80217af7f095df546c8d964250c70f
Carlos Andres Ramirez <91298237+sacrabma92@users.noreply.github.com>
2024-06-24 13:16:12 -0500

    Base

--- spring-security-course/src/main/java/com/curso/api/spring_security_course/service/CategoryService.java
+++ spring-security-course/src/main/java/com/curso/api/spring_security_course/service/CategoryService.java
@@ -1,7 +1,8 @@
 package com.curso.api.spring_security_course.service;
 
-import com.curso.api.spring_security_course.dto.SaveProduct;
+import com.curso.api.spring_security_course.dto.SaveCategory;
 import com.curso.api.spring_security_course.persistence.entity.Category;
+import jakarta.validation.Valid;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
@@ -13,9 +14,9 @@ public interface CategoryService {
 
    Optional<Category> findOneById(Long categoryId);
 
-   Category createOne(SaveProduct saveProduct);
+   Category createOne(@Valid SaveCategory saveProduct);
 
-   Category updateOneById(Long categoryId, SaveProduct saveProduct);
+   Category updateOneById(Long categoryId, @Valid SaveCategory saveProduct);
 
    Category disableOneById(Long categoryId);
 }
