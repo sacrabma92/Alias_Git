9aedd5454c664c5b69b1aa56f8c47901ea53bb7a
Carlos Andres Ramirez <91298237+sacrabma92@users.noreply.github.com>
2024-06-29 11:55:28 -0500

    Mapper

---
+++ JavaMappers/src/main/java/org/example/ModelMapper/Dto/PersonCustomDTO.java
@@ -0,0 +1,10 @@
+package org.example.ModelMapper.Dto;
+
+public class PersonCustomDTO {
+   private Long idDTO;
+   private String nameDTO;
+   private String lasNameDTO;
+   private String emailDTO;
+   private Byte ageDTO;
+   private Character genderDTO;
+}
---
+++ JavaMappers/src/main/java/org/example/ModelMapper/Entities/Person.java
@@ -0,0 +1,70 @@
+package org.example.ModelMapper.Entities;
+
+public class Person {
+   private Long id;
+   private String name;
+   private String lasName;
+   private String email;
+   private Byte age;
+   private Character gender;
+
+   public Person() {
+   }
+
+   public Person(Long id, String name, String lasName, String email, Byte age, Character gender) {
+      this.id = id;
+      this.name = name;
+      this.lasName = lasName;
+      this.email = email;
+      this.age = age;
+      this.gender = gender;
+   }
+
+   public Long getId() {
+      return id;
+   }
+
+   public void setId(Long id) {
+      this.id = id;
+   }
+
+   public String getName() {
+      return name;
+   }
+
+   public void setName(String name) {
+      this.name = name;
+   }
+
+   public String getLasName() {
+      return lasName;
+   }
+
+   public void setLasName(String lasName) {
+      this.lasName = lasName;
+   }
+
+   public String getEmail() {
+      return email;
+   }
+
+   public void setEmail(String email) {
+      this.email = email;
+   }
+
+   public Byte getAge() {
+      return age;
+   }
+
+   public void setAge(Byte age) {
+      this.age = age;
+   }
+
+   public Character getGender() {
+      return gender;
+   }
+
+   public void setGender(Character gender) {
+      this.gender = gender;
+   }
+}
