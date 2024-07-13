835778dc7cd74d23278a28d9ddf58d0d20940684
Carlos Andres Ramirez <91298237+sacrabma92@users.noreply.github.com>
2024-07-13 10:55:34 -0500

    Eliminado

--- spring-security-course/src/main/java/com/cursos/api/springsecuritycourse/persistence/repository/ProductRepository.java
+++
@@ -1,7 +0,0 @@
-package com.cursos.api.springsecuritycourse.persistence.repository;
-
-import com.cursos.api.springsecuritycourse.persistence.entity.Product;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProductRepository extends JpaRepository<Product, Long> {
-}
